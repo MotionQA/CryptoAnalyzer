afaa9524805f607be672305fa4a3dd792d4050d5
MotionQA <keygo@mail.ru>
2025-07-20 18:52:49 +0300

    first code

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,8 +3,10 @@ import entity.Result;
 public class Application {
     public Application (String [] args){
 
-        public Result run() {
-            return null;
+
+
+    public static Result run() {
+        return null;
         }
     }
 }
