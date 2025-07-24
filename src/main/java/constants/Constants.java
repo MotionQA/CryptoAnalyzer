0134f46c00e07a5dc4671762332a598a7418c03e
MotionQA <keygo@mail.ru>
2025-07-24 19:55:07 +0300

    ++

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,9 +1,12 @@
 package constants;
 
+import java.io.File;
+
 public class Constants {
     private static final String rus = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ";
     private static final String eng = "QWERTYUIOPASDFGHJKLZXCVBNM";
     private static final String cypher = "123456789";
     private static final String z = "!:!@#^&*()_+{}:|<>?$%^";
     public static final String ALPHABET = rus + eng + rus.toLowerCase() + eng.toLowerCase() + cypher + z;
+    public static final String TXT_FOLDER=System.getProperty("user.dir")+ File.separator+"text"+File.separator;
 }
